d8c76be66038968a5767132eb4e94ee9a52d9323
rii-commits <riisaesp@gmail.com>
2026-06-11 15:52:17 -0300

    Diagnosticar inicialização da API na Vercel

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
-const { handleApi } = require("../src/handler");
-
 module.exports = async function handler(req, res) {
-  return handleApi(req, res);
+  try {
+    const { handleApi } = require("../src/handler");
+    return await handleApi(req, res);
+  } catch (error) {
+    console.error(error);
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+    res.end(JSON.stringify({
+      error: "Falha ao iniciar a API.",
+      detail: error?.message || String(error)
+    }));
+  }
 };
